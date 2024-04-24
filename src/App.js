e6ea5ddb4d3dbdaf3b1b7c1fda5e9ea8556ea873
pmason <pmason@argenx.com>
2024-04-24 08:02:03 +0100

    mui graphs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grid, Box } from "@mui/material";
+import { BarChart, LineChart, PieChart, SparkLineChart } from "@mui/x-charts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={2}>
+      <Grid item xs={6} md={6}>
+        <BarChart
+          series={[
+            { data: [35, 44, 24, 34] },
+            { data: [51, 6, 49, 30] },
+            { data: [15, 25, 30, 50] },
+            { data: [60, 50, 15, 25] },
+          ]}
+          height={290}
+          xAxis={[{ data: ["Q1", "Q2", "Q3", "Q4"], scaleType: "band" }]}
+          margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+        />
+      </Grid>
+      <Grid item xs={6} md={6}>
+        <LineChart
+          xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+          series={[
+            {
+              data: [2, 5.5, 2, 8.5, 1.5, 5],
+            },
+          ]}
+          height={300}
+          margin={{ left: 30, right: 30, top: 30, bottom: 30 }}
+          grid={{ vertical: true, horizontal: true }}
+        />
+      </Grid>
+      <Grid item xs={6} md={6}>
+        <PieChart
+          series={[
+            {
+              data: [
+                { id: 0, value: 10, label: "series A" },
+                { id: 1, value: 15, label: "series B" },
+                { id: 2, value: 20, label: "series C" },
+              ],
+            },
+          ]}
+          width={400}
+          height={200}
+        />
+      </Grid>
+      <Grid item xs={6} md={6}>
+        <Box sx={{ flexGrow: 1 }}>
+          <SparkLineChart data={[1, 4, 2, 5, 7, 2, 4, 6]} height={100} />
+        </Box>
+        <Box sx={{ flexGrow: 1 }}>
+          <SparkLineChart
+            plotType="bar"
+            data={[1, 4, 2, 5, 7, 2, 4, 6]}
+            height={100}
+          />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
-
 export default App;
