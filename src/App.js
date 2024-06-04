155bcbdd1aad75471ed23a4e45f7b1417ccbc061
pmason <pmason@argenx.com>
2024-06-04 14:16:22 +0100

    experimental

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, Box } from "@mui/material";
 import { BarChart, LineChart, PieChart, SparkLineChart } from "@mui/x-charts";
+// import { ReactSVG } from "react-svg";
+// import { ReactComponent as YourSvg } from "../src/svg/gadam_jobs_flow.svg";
 
 function App() {
+  // const [svg, setSvg] = useState(null);
+
+  // // fetch svg file
+  // useEffect(() => {
+  //   fetch("./svg/test.svg")
+  //     .then((response) => response.text())
+  //     .then((svg) => {
+  //       console.log(svg);
+  //       setSvg(svg);
+  //     });
+  // });
+
   return (
     <Grid container spacing={2}>
+      <Grid item xs={12} md={12}>
+        {/* <ReactSVG src="test.svg" />
+        {svg} */}
+        {/* <YourSvg /> */}
+        {/* <img
+          src={require("../src/svg/gadam_jobs_flow.svg").default}
+          alt="mySvgImage"
+        /> */}
+        <iframe
+          src="https://mermaid.ink/svg/pako:eNrlWsuS4jYU_RVKs5vC-AHG4KokmyyTTbILpFxCEkaDbTGSTDfT1f8eyaIb5Da0VwExm26wruVzzn3oyuIFIIYJSMG6YE9oA7kc_PHXsgoX39gqyyGGZcaqnNEqz4SsMSVipEb-9bxfo8bkeFH9h7I2Y8sKFRRtB-FgCTZS7kTq-8-Q7zjDI1ZhUsIKjwQUI8RKvxBw7T-RFYZ7n5Md83NSEQ4Lf0WZntOXjBXCX9OC7Cl5Itynaorn0UaWxW_66i8f7lAYhN8J3cdkb4avsluCXgKMz0wElmW2JYcMMU6yPeS2FGMnpOgm0VONSWNSVz_ozkwkWM0RsXWY3LkO3fB7KjBtTDT6LCcyK6CQzVSWAtM7V6Abfk8FouDM5jvK2J5wDdAOgii4vQacrGpa4Ex_6UqFNvi-AhibEm6JiaKdApnpuW0F7r00XiDQV4XolAnN_d2pEEUu5MJHAn1VmJzZcLLmRGyMpm-RZasxuYesOEOpOXelxkUqfXWZLlR9wVBCm_-tKmNuagBDdUkq-SYFFpvV8SPZm-sn0H2ZxsZGVdPcrCmXm6Uovr9k0BjOvH-FhhbkyOPOc_oadte7tKstmIpHE456lDxLDpHMNB47Dm8ahrWkRTvsOuGefOYO3qvtofJOshBE1rtMA-RSB0FTeLI1KzDhlpOSW5K-hvIUr59x6aPI7NICqi0tQWYurp1vLPpIMT-Tot6pdYi8z0WrNbPEmDsixgUefeQIgwVileo9ZIbJmlZEMk3Mevtw012GjS5SjzspcAF5L97nXYdeIw5NntnE3XjvYqPvRT6y6sFpA2fTj-5_d9kGf1rOHqSyP0hVfph66nBJdLyoXS9auqqNe1W1_2tvoh5XUaRukURFmRqmyEeYCuTtaMGkFwRB-D6d-pMrRcxjKDL1rketu85Zi2K2MhB_tVWYOKVCP6pmV_YdNbGjR1p9TOye59tk-glh2n20IWibKW5ZwSC2pZi5J8VHOv3EMA2_bMX_3Ln4dzdxL-Slds70OHR04F6Ywm_7aupi3nYw-kyMpFsMr_Mtd5g8gCpe-633A3r8J3DXw6wmzi4OQh4KooGriYv0C0JoiFjBePoljuOhkJxtiYeh2EDO4SEdxIPY9UNk9w-APznd1ede48X7rd5Xz-5hons-ZTnB1rHtNbHttWI7aLny0fg8xDn0gx2qOnY2DoagJLyEFIMUvCyrgYIvN6QkS5Cqj2umHCe14auyhLVkfx8qBFLJazIE5jXW7xTmHJZvF3ew-oex968EU8n4n-aXi80PGBsTkL6AZ5COR7MoSWbjeTiO4kkyngzBAaReHI7iJIjDKAmDaRjMXofgRzNnMJrP5rM4mEzDyWysVq9kCDir8w1I17AQ5PU_5fRvjg"
+          title="gadam_jobs_flow"
+          height="400"
+          width="1000"
+        />
+      </Grid>
       <Grid item xs={6} md={6}>
         <BarChart
           series={[
